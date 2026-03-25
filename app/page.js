731dc4ed0d713790ecf7ea0dad2a0b8006665962
Dimuth Adithya <dimuthx353@gmail.com>
2026-03-25 16:16:37 +0530

    feat: replace static heading with animated text component

--- app/page.js
+++ app/page.js
@@ -1,16 +1,19 @@
 import Image from 'next/image';
 import { WordRotate } from '@/components/ui/word-rotate';
+import { TextAnimate } from '@/components/ui/text-animate';
 
 export default function Home() {
   return (
     <>
-      <div className='font-inconsolata flex gap-2 h-screen items-center justify-center tracking-tight text-red-700 bg-neutral-950 '>
-        {/* <WordRotate
-          words={['Dimuth', 'adithya']}
-          className={'text-4xl font-bold uppercase'}
-        /> */}
-        <h6 className='text-4xl font-bold uppercase'>dimuth</h6>
-        <div className='tracking-tighter flex flex-col text-sm font-extralight justify-center opacity-80 transition-all duration-150 ease-in-out w-20'>
+      <div className='font-inconsolata flex gap-2 h-screen items-center justify-center tracking-tight  bg-neutral-950 '>
+        <TextAnimate
+          animation='slideLeft'
+          by='character'
+          className={'text-red-600 text-4xl font-bold uppercase'}
+        >
+          Dimuth
+        </TextAnimate>
+        <div className='tracking-tighter flex flex-col text-sm font-extralight justify-center opacity-80 transition-all duration-150 ease-in-out w-20 text-red-700'>
           <span>Portfolio</span>
           <span className='-mt-1'>
             <span className='me-1'>©</span>
