0a14e4a7ee651fa9df87a007de583e8e4385e98b
Dimuth Adithya <dimuthx353@gmail.com>
2026-03-27 16:44:03 +0530

    feat: update WobbleCardDemo component with new content and add demo video

--- components/wobble-card-demo.jsx
+++ components/wobble-card-demo.jsx
@@ -12,11 +12,11 @@ export default function WobbleCardDemo() {
       >
         <div className='max-w-xs'>
           <h2 className='text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white'>
-            Gippity AI powers the entire universe
+            Track Your YouTube Learning Journey with YTPlayer Tracker
           </h2>
           <p className='mt-4 text-left  text-base/6 text-neutral-200 max-w-[80%]'>
-            With over 100,000 mothly active bot users, Gippity AI is the most
-            popular AI platform for developers.
+            Track YouTube learning, organize playlists as courses, and manage
+            your progress with YTPLAYTracker’s dashboard and Chrome Extension.
           </p>
         </div>
         <video
@@ -31,22 +31,25 @@ export default function WobbleCardDemo() {
         />
       </WobbleCard>
       <WobbleCard containerClassName='col-span-1 min-h-[300px]'>
-        <h2 className='max-w-80  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white'>
-          No shirt, no shoes, no weapons.
-        </h2>
-        <p className='mt-4 max-w-[26rem] text-left  text-base/6 text-neutral-200'>
-          If someone yells “stop!”, goes limp, or taps out, the fight is over.
-        </p>
+        <video
+          src='/videos/projects/tinayastore.webm'
+          width={450}
+          height={100}
+          alt='linear demo image'
+          className='absolute right-[0%] -bottom-2 object-contain rounded-2xl'
+          autoPlay
+          loop
+          muted
+        />
       </WobbleCard>
       <WobbleCard containerClassName='col-span-1 lg:col-span-3 bg-blue-900 min-h-[500px] lg:min-h-[600px] xl:min-h-[300px]'>
         <div className='max-w-sm'>
           <h2 className='max-w-sm md:max-w-lg  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white'>
-            Signup for blazing-fast cutting-edge state of the art Gippity AI
-            wrapper today!
+            Busy Books: Your Ultimate Digital Library
           </h2>
           <p className='mt-4 max-w-[26rem] text-left  text-base/6 text-neutral-200'>
-            With over 100,000 mothly active bot users, Gippity AI is the most
-            popular AI platform for developers.
+            Discover, read, and manage your favorite books effortlessly with
+            Busy Books, your smart online library companion.
           </p>
         </div>
         <video
